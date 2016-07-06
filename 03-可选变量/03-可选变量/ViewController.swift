bf97f4a3d6f6eeae07e35439cee91757f818784e
Alex Lau <429095816@qq.com>
2016-07-06 20:14:35 +0800

    03

---
+++ 03-可选变量/03-可选变量/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  03-可选变量
+//
+//  Created by lwb on 16/7/6.
+//  Copyright © 2016年 lwb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var value1:Int?
+        print(value1)
+        let value2:Int?=nil
+        print(value2)
+        let value3:Int?=66
+        print(value3)
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
